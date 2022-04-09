df95c59bd11a71da0a3495e05868081fafba0300
Tkachmaxim <tkachmaxim23@gmail.com>
2022-04-09 10:03:49 +0300

    f1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
+import user from "../user.json"
+import data from "../data.json"
+import friends from "../friends.json"
+import transaction from "../transactions.json"
+import Profile from "./Profile";
+import Statistics from "./Statistics";
+import FriendList from "./FriendList";
+import TransactionHistory from "./TransactionHistory";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      Hello
+    <div>
+      <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats}/>
+      <Statistics title="Statistics" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transaction} />
     </div>
   );
 };
